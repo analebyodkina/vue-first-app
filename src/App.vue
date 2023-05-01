cd21b080197483dc96afdce9f7a6c41375815d4f
analebyodkina <dp030992cad@gmail.com>
2023-05-01 14:49:05 +0300

    init new repository. created new ve app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "assets/scss/index.scss";
+</style>
